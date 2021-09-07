bdb4b259dc69c674e84d54c625df24fa14266a21
Will Young <numino@hotmail.co.uk>
2021-09-07 23:24:59 +0100

    Added test project. Started api controller to create raids.

---
+++ RaidMaker.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace RaidMaker.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ RaidMaker/Api/RaidController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaidMaker.Api
+{
+    public class RaidController : Controller
+    {
+        [HttpGet("api/raid/{reference}")]
+        public void Do(string reference)
+        {
+            
+        }
+
+        [HttpPost("api/raid")]
+        public void Post()
+        {
+            
+        }
+    }
+}
